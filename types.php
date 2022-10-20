5c35526c0ec573109393d591d1ec1b146963f84f
Dushyant Vishwakarma <dushyantss8@gmail.com>
2022-10-20 23:19:55 +0530

    Data Types

---
+++ types.php
@@ -0,0 +1,29 @@
+<?php
+define("ENDL","<br>");
+
+/* Data Types & Type Casting */
+
+# PHP has 4 scalar types
+    # bool = true / false
+    $completed = true;
+    # int = 1,2,-1,-2 (no decimal)
+    $score = 75;
+    # float = 1.5, 0.1, 3.1456
+    $price = 55.55;
+    # string = 'Hello', "Bye"
+    $greeting = "Hello World";
+
+    echo $completed . ENDL;
+    echo $score . ENDL;
+    echo $price . ENDL;
+    echo $greeting . ENDL; 
+
+# 4 Compound Types 
+    # array 
+    # object
+    # callable
+    # iterable
+
+# 2 Special Types
+    # resource 
+    # null
