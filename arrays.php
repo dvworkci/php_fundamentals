90d72af448cf3ee2ffeabacacc14b3bcd431fa56
Dushyant Vishwakarma <dushyantss8@gmail.com>
2022-10-25 19:02:55 +0530

    Operators

--- arrays.php
+++ arrays.php
@@ -36,12 +36,63 @@ $versions = [
 $versions = [
     'php' => [
         'creator' => 'Rasmus Lerdorf',
-        'version' => 8.1
+        'versions' => [
+            ['version' => 8, 'releaseDate' => 'Nov 26, 2020'],
+            ['version' => 7.4, 'releaseDate' => 'Nov 28, 2019'],
+        ]
     ],
     'python' => [
         'creator' => 'Guido Van Rossum',
-        'version' => 3.6
+        'versions' => [
+            ['version' => 3.9, 'releaseDate' => 'Oct 5, 2020'],
+            ['version' => 3.8, 'releaseDate' => 'Oct 14, 2019'],
+        ]
     ]
 ];
 
 print_r($versions) . EL;
+echo $versions['php']['creator'] . EL;
+echo $versions['php']['versions'][0]['releaseDate'] . EL;
+
+// If two array items have the same key then PHP will override the previous one
+$arr2 = [0 => 'foo',1 => 'bar',1 => 'baz'];
+
+// PHP will try to cast keys when possible, it is recommended to declare keys only in one data type 
+// key type in null is casted to an empty string
+$arr3 = [true => 'a',1 => 'b', '1' => 'c',1.8 => 'd'];      // Array([1] => d)
+
+print_r($arr2) . EL;
+print_r($arr3) . EL;
+
+$arr4 = ['a','b',50 => 'c','d','e'];
+// array_pop() = removes the last element of an array and returns it
+echo array_pop($arr4) . EL;
+print_r($arr4) . EL;
+
+// array_shift() = removes the first element of an array and returns it
+echo array_shift($arr4) . EL;
+print_r($arr4) . EL;
+
+$arr5 = [1,2,3];
+// we can use unset to destroy whole array or any of its element
+unset($arr5[0]);
+print_r($arr5) . EL;
+// unset($arr5);
+// But, if we destroy only keys,
+unset($arr5[0],$arr5[1],$arr5[2]);
+$arr5[] = 6;            // Array(3) => 6, because array in PHP after getting destroyed retains its last key 
+print_r($arr5) . EL;
+
+// CASTING AN SCALAR TYPE INTO AN ARRAY
+$x = 5;
+var_dump((array) $x) . EL;
+
+// DIFFERENCE BETWEEN isset() and array_key_exists()
+$arr6 = ['a' => 1,'b' => 2,'c' => null];
+
+// array_key_exists() will tell you if a particular key exists in an array regardless of its value
+var_dump(array_key_exists('c',$arr6)) . EL;
+
+// isset() will tell you if a particular key exists and not set to non-empty value
+var_dump(isset($arr6['c'])) . EL;
+
---
+++ operators.php
@@ -0,0 +1,120 @@
+<?php
+define("EL","\n");
+
+// ARITHMETIC OPERATORS (+ - * / %)
+$x = 10;
+$y = 2;
+
+// var_dump($x + $y) . EL;
+// var_dump($x - $y) . EL;
+// var_dump($x * $y) . EL;
+// var_dump($x / $y) . EL;
+// var_dump($x % $y) . EL;
+
+// Some tips about arithmetic operators
+// Division operator => The type of the result will always be float unless both of the operators are integers and they are evenly divisible
+$num1 = 10;
+$num2 = 3;
+$num3 = 2.0;
+
+// var_dump($num1 / $num2) . EL;
+// var_dump($num1 / $num3) . EL;
+
+// Modulus operator => In this operator the operands are casted into integer data type
+$num4 = 10.5;
+
+// var_dump($num4 % $num3) . EL;       // 10 % 2 instead of 10.5 % 2.0
+// // If you want to perform mod operations on floating numbers use fmod() function
+// var_dump(fmod($num4,$num3)) . EL;
+
+// Note: If using modulus in negative numbers then the sign of result will depend on the sign of the dividend only
+$num5 = -10;
+$num6 = 3;
+
+// var_dump($num5 % $num6) . EL;       // Result = -1
+
+// ASSIGNMENT OPERATORS (= += -= *= /= %= **=)
+$x = $y = $z = 23;
+// var_dump($x,$y,$z) . EL;
+
+// STRING OPERATORS (. .=)
+// Concatenation
+$msg1 = "Hello";
+$msg2 = "World";
+$message = $msg1 . " " . $msg2;
+// echo $message . EL;
+
+// COMPARISON OPERATORS (==, ===, !=, !== <>, <, >, <=, >=, <=>, ??, ?:)
+$a = '5';
+$b = 5;
+
+// var_dump($a == $b);     // Loose equal
+// var_dump($a === $b);    // Strict equal
+
+// var_dump($a != $b);    // Loose not equal
+// var_dump($a !== $b);    // Strict not equal
+
+// var_dump($a < $b);    // lesst than
+// var_dump($a > $b);    // greater than
+
+// var_dump($a <= $b);    // Less than or equal to
+// var_dump($a >= $b);    // Greater than or equal to
+
+// Spaceship operator
+// var_dump(5 <=> 5);    // Returns 0
+// var_dump(1 <=> 5);    // Returns -1 if a < b
+// var_dump(10 <=> 5);    // Returns 1 if a > b
+
+// Error control operators (@)
+$x = @file('foo.txt');   // File does not exist error, so suppress this error using @
+
+// Increment and Decrement Operators (++, --)
+$y = 5;
+$res = $y++;
+$res2 = ++$y;
+
+// Logical operators (&& || ! and or xor)
+$ab = true;
+$cd = false;
+
+// &&
+// var_dump($ab && $cd);
+// var_dump($ab || $cd);
+// var_dump(!$ab);
+
+// Bitwise operators (& | ^ ~ << >>) Used to perform bit level operations
+$ef = 6;
+$gh = 3;
+
+// $ef & $gh means 6 in binary & 4 in binary
+// 110 & 011
+//  110
+//  011
+//= 010 = 2 
+// var_dump($ef & $gh);    // int(2)
+
+// $ef | $gh means 
+// 110 | 011
+//  110
+//  011
+//= 111 = 7
+// var_dump($ef | $gh);        // int(7)
+
+// $ef ^ $gh means  ^ => XOR operations
+// 110 ^ 011
+//  110
+//  011
+//= 101 = 5
+// var_dump($ef ^ $gh);        // int(5)
+
+// Array Operators (+ == === != <> !==)
+$arr1 = ['a','b','c','d'];
+$arr2 = ['e','f','g','h','i'];
+
+// The + operator returns the right-hand array appended to the left-hand array; for keys that exist in both arrays, the elements from the left-hand array will be used, and the matching elements from the right-hand array will be ignored.
+$resArray = $arr1 + $arr2;
+print_r($resArray);
+
+
+
+
